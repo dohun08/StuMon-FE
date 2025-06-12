81977cf5ae6bd009aedbb9d389cf0e3245a05d34
dohun08 <bemik624@gmail.com>
2025-06-12 23:53:02 +0900

    feat(#2) :: 이석 페이지 틀

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import {
   Route
 } from "react-router-dom"
 import App from './App.tsx'
+import LeaveSeat from "./pages/leaveseat";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/Leaveseat" element={<LeaveSeat />} />
+      <Route path="*" element={<div>404</div>} />
     </Routes>
   </BrowserRouter>
 )
---
+++ src/pages/leaveseat/index.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "../../components/layout/RootLayout";
+
+
+export default function LeaveSeat() {
+  return (
+    <RootLayout>
+      <div>LeaveSeat</div>
+    </RootLayout>
+  )
+}
