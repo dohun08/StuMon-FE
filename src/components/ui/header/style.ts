884f7dd56fd4e9fa5cb8b8a1a6c4503224a3f460
zuu3 <ojh27467242@gmail.com>
2025-06-13 00:05:07 +0900

    feat(#1) :: 헤더 컴포넌트 완성

--- src/components/ui/header/style.ts
+++ src/components/ui/header/style.ts
@@ -6,7 +6,6 @@ export const HeaderContainer = styled.header`
   padding: 1.19rem;
   color: #6C757D;
   display: flex;
-  align-items: center;
   flex-direction: column;
 `;
 
@@ -25,11 +24,19 @@ export const Menu = styled.nav`
   gap: 1.31rem;
 `;
 export const MenuItem = styled.div<{ active?: boolean }>`
-  color: ${({ active }) => (active ? "#0D6EFD" : "#6C757D")};
-  background-color: ${({ active }) => (active ? "#E9F2FF" : "transparent")};
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${({ active }) => (active ? "#2E6FF2" : "")};
+  background: ${({ active }) => (active ? "#C7DAFF" : "transparent")};
   padding: 0.5rem 1rem;
   border-radius: 0.5rem;
   font-size: 1.125rem;
   font-weight: 600;
   cursor: pointer;
+
+  & > img {
+    filter: ${({ active }) => active ? "brightness(0) saturate(100%) invert(38%) sepia(87%) saturate(5805%) hue-rotate(206deg) brightness(96%) contrast(101%)" : "none"};
+    transition: filter 0.2s;
+  }
 `;
