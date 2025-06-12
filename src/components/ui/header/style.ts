98fcc6201adc2660b980a536fff78db33bf591b1
zuu3 <ojh27467242@gmail.com>
2025-06-12 23:21:11 +0900

    refactor(#1) :: 헤더 컴포넌트 크기 조절 수정

--- src/components/ui/header/style.ts
+++ src/components/ui/header/style.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 export const HeaderContainer = styled.header`
+  width: 12.875rem;
   background-color: #EBF1FF;
   padding: 1.19rem;
   color: #6C757D;
@@ -10,16 +11,17 @@ export const HeaderContainer = styled.header`
 `;
 
 export const Logo = styled.img`
-  width: 14.875rem;
-  height: 3.25038rem;
-  vertical-align: middle;
+  width: 100%;
 `;
 export const Divider = styled.div`
-  width: 14.875rem;
-  height: 0.125rem;
+  width: 100%;
+  border-bottom: 2px solid #DFE6FD;
   flex-shrink: 0;
-  stroke-width: 1px;
-  stroke: #DFE6FD;
+  margin: 2.7rem 0;
 `;
-export const Menu = styled.nav``;
+export const Menu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 1.31rem;
+`;
 export const MenuItem = styled.div``;
