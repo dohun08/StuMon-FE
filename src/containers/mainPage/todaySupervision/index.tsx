5e528d7acac7d0f60b6dc65fb3a422f20d66c9d8
zuu3 <ojh27467242@gmail.com>
2025-06-29 11:27:06 +0900

    feat(#26) :: 오늘의 자습감독 반영

--- src/containers/mainPage/todaySupervision/index.tsx
+++ src/containers/mainPage/todaySupervision/index.tsx
@@ -7,7 +7,6 @@ import Moon from '../../../assets/icons/moon.svg';
 
 export default function TodaySupervision() {
   const { data } = useGetTodaySupervision(new Date().toISOString().slice(0, 10));
-  console.log('Today supervision data:', data);
   return (
     <S.Wrapper>
       <h2>오늘의 감독</h2>
@@ -17,21 +16,21 @@ export default function TodaySupervision() {
             <img src={StudyLamp} alt="Study Lamp" />
             <span>자습 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{"이혜정"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{data?.self_study_teacher["8th_teacher"] || "X"}</S.SupervisionTeacher>
         </S.Supervision>
         <S.Supervision>
           <S.SupervisionTitle>
             <img src={leaveSeatChange} alt="Leave Seat Change" />
             <span>이석 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{"최병준"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{data?.leave_seat_teacher["8th_teacher"] || "X"}</S.SupervisionTeacher>
         </S.Supervision>
         <S.Supervision>
           <S.SupervisionTitle>
             <img src={Moon} alt="Moon" />
             <span>야간 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{"진예빈"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{data?.night_teacher || "X"}</S.SupervisionTeacher>
         </S.Supervision>
       </S.Info>
     </S.Wrapper>
