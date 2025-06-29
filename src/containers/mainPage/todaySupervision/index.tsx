56da54a5cda320d564b9e182136c3615901939e5
zuu3 <ojh27467242@gmail.com>
2025-06-29 14:40:30 +0900

    refactor(#26) :: undefined 처리

--- src/containers/mainPage/todaySupervision/index.tsx
+++ src/containers/mainPage/todaySupervision/index.tsx
@@ -6,7 +6,13 @@ import Moon from '../../../assets/icons/moon.svg';
 
 
 export default function TodaySupervision() {
-  const { data } = useGetTodaySupervision(new Date().toISOString().slice(0, 10));
+  const { data, isLoading } = useGetTodaySupervision(new Date().toISOString().slice(0, 10));
+  if (isLoading) {
+    return <S.Wrapper>로딩 중...</S.Wrapper>;
+  }
+  const selfStudyTeacher = data?.self_study_teacher?.['8th_teacher'] ?? 'X';
+  const leaveSeatTeacher = data?.leave_seat_teacher?.['8th_teacher'] ?? 'X';
+  const nightTeacher = data?.night_teacher ?? 'X';
   return (
     <S.Wrapper>
       <h2>오늘의 감독</h2>
@@ -16,21 +22,21 @@ export default function TodaySupervision() {
             <img src={StudyLamp} alt="Study Lamp" />
             <span>자습 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{data?.self_study_teacher["8th_teacher"] || "X"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{selfStudyTeacher}</S.SupervisionTeacher>
         </S.Supervision>
         <S.Supervision>
           <S.SupervisionTitle>
             <img src={leaveSeatChange} alt="Leave Seat Change" />
             <span>이석 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{data?.leave_seat_teacher["8th_teacher"] || "X"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{leaveSeatTeacher}</S.SupervisionTeacher>
         </S.Supervision>
         <S.Supervision>
           <S.SupervisionTitle>
             <img src={Moon} alt="Moon" />
             <span>야간 감독</span>
           </S.SupervisionTitle>
-          <S.SupervisionTeacher>{data?.night_teacher || "X"}</S.SupervisionTeacher>
+          <S.SupervisionTeacher>{nightTeacher}</S.SupervisionTeacher>
         </S.Supervision>
       </S.Info>
     </S.Wrapper>
